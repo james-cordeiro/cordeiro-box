4b29dc61472cdb45ccba7583e003855ed4431324
James Cordeiro <james-cordeiro@users.noreply.github.com>
2015-06-25 03:26:03 +0100

    fixed bug in node app.js response

--- puphpet/files/exec-once/run.sh
+++ puphpet/files/exec-once/run.sh
@@ -22,7 +22,7 @@ sudo tee app.js > /dev/null <<EOF
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello Node\n’);
+  res.end("Hello Node\n");
 }).listen(3000, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:3000/');
 EOF
