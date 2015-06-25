8f2be1e1e7344e88b9a555a0226fc79e5f6636f4
James Cordeiro <james-cordeiro@users.noreply.github.com>
2015-06-25 02:25:59 +0100

    added app.js file creation and cleanup

--- puphpet/files/exec-once/run.sh
+++ puphpet/files/exec-once/run.sh
@@ -10,8 +10,6 @@ sudo /usr/share/webmin/changepass.pl /etc/webmin root vagrant
 
 
 # CONFIGURE WEB & APP SERVER
-
-
 sudo mkdir /usr/share/nginx/php
 sudo mkdir /usr/share/nginx/php/html
 cd /usr/share/nginx/php/html
@@ -20,9 +18,20 @@ echo "<?php phpinfo(); ?>" | sudo tee index.php
 sudo mkdir /usr/share/nginx/node
 sudo mkdir /usr/share/nginx/node/html
 cd /usr/share/nginx/node/html
-#echo "<?php php_info(); ?>" | sudo tee index.php
+sudo tee app.js > /dev/null <<EOF 
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello Node\n’);
+}).listen(3000, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:3000/');
+
+
+EOF
 
 #sudo npm install pm2 -g --unsafe-perm 2&>1 >/dev/null
-cd /usr/share/nginx/node/ && sudo pm2 start app.js
+cd /usr/share/nginx/node/html && sudo pm2 start app.js
+
+sudo rm -r /usr/share/nginx
 
 
